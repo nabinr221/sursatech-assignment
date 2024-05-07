e609054b5a257920da315dbdda490214067de862
nabinr221 <nabinr221@gmail.com>
2024-05-07 17:26:00 +0545

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sursa Tech Assignments",
+  description: "Assignments of Sursa Tech for frontend developer ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
